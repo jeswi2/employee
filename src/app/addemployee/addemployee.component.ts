1aae5de37214c958ea5dc805beb09afd3a955bd8
jeswi2 <jeswimathewm@gmail.com>
2022-10-13 15:58:34 +0530

    de

--- src/app/addemployee/addemployee.component.ts
+++ src/app/addemployee/addemployee.component.ts
@@ -9,6 +9,42 @@ export class AddemployeeComponent implements OnInit {
 
   constructor() { }
 
+
+  employeecode=""
+  employee=""
+  designation=""
+  gender=""
+  company_name=""
+  salary=""
+  address=""
+  number=""
+  email=""
+  experiance=""
+  blood=""
+  dob=""
+
+
+  readvalues=()=>{
+    let data={
+
+      "employeecode":this.employeecode,
+      "employee":this.employee,
+      "designation":this.designation,
+      "gender":this.gender,
+      "company_name":this.company_name,
+      "salary":this.salary,
+      "address":this.address,
+      "number":this.number,
+      "email":this.email,
+      "experiance":this.experiance,
+      "blood":this.blood,
+      "dob":this.dob,
+    }
+    console.log(data)
+
+  
+}
+
   ngOnInit(): void {
   }
 
